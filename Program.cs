c1589e95eb90d968a3c5a4c79baa0b19f88d7060
RyanDynamix <ryannguyen070823@gmail.com>
2023-04-18 17:33:46 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MoneyExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 23000;
+            Console.Write("Enter how mush USD$ you would like to exchange : ");
+            float USD = float.Parse(Console.ReadLine());
+            Console.WriteLine("You exchanged "+x+"$ to "+USD * x+ "VND");
+        }
+    }
+}
